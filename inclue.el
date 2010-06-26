bf086270294a9817940b3bdd36615dc68ad1e19f
Thomas Munro <macdice@penski.net>
2010-06-26 21:56:56 +0100

    First cut of c99 support

--- inclue.el
+++ inclue.el
@@ -1,4 +1,4 @@
-;;; inclue.el --- A simple-minded #include helper for C++
+;;; inclue.el --- A simple-minded #include helper for C and C++
 
 ;;; Commentary:
 ;; 
@@ -50,49 +50,97 @@
       ;; TODO find alphabetical insertion point
       (insert new-line)
       (insert "\n"))))
-        
-(defvar inclue-mappings (make-hash-table :test 'equal)
-  "A hash-table of indentifier -> header.")
 
-(defvar inclue-documentation (make-hash-table :test 'equal)
-  "A hash-etable of identifier -> documentation.")
+(defstruct inclue-library
+  "A record type to hold documentation and header info."
+  (identifier)
+  (include-style 'angle-brackets)
+  (names)                                               ;; list of strings
+  (header-table (make-hash-table :test 'equal))         ;; string -> string
+  (documentation-table (make-hash-table :test 'equal))) ;; string -> string
 
-(defun inclue-load-mappings (header identifier-documentation-pairs)
+(defvar inclue-library-table (make-hash-table :test 'eq)
+  "A hash-table of library identifier symbol -> inclue-library.")
+
+(defvar inclue-active-libraries '()
+  "A list of active library identifiers (symbols).  For example,
+  this could be set to the list (c-99 posix-1003) while
+  programming in C, and the list (c++-03 posix-1003) while
+  programming in C++, possibly in a mode hook.  TODO: make buffer
+  local")
+
+(defun inclue-load-mappings (library-identifier 
+                             header 
+                             identifier-documentation-pairs)
   "Load LIST consisting of header name followed by identifier strings."
-  (loop for (identifier documentation) in identifier-documentation-pairs 
-        as identifier-name = (symbol-name identifier) do 
-        (setf (gethash identifier-name inclue-mappings) header)
-        (setf (gethash identifier-name inclue-documentation) documentation)))
+  (let ((library (gethash library-identifier inclue-library-table)))
+    (unless library
+      (error "inclue-load-mappings -- unknown library %s" library-identifier))
+    (loop 
+     for (identifier doc-string) in identifier-documentation-pairs 
+     as name = (symbol-name identifier) do 
+     (push name (inclue-library-names library))
+     (setf (gethash name (inclue-library-header-table library))
+           header)
+     (setf (gethash name (inclue-library-documentation-table library))
+           doc-string))))
 
-(defmacro define-inclue-standard-header (header doc-string &rest identifier-pairs)
-  `(inclue-load-mappings ,header ',identifier-pairs))
+(defmacro* define-inclue-library ((identifier include-style) doc-string)
+  `(setf (gethash ',identifier inclue-library-table)
+         (make-inclue-library :identifier ',identifier
+                              :include-style ',include-style)))
+
+(defmacro define-inclue-header (desc doc-string &rest identifier-pairs)
+  `(inclue-load-mappings ',(first desc) ,(second desc) ',identifier-pairs))
 
 (defun inclue-add-header-for-identifier-at-point ()
   "Add the appropriate header for the C++ identifier at point."
   (interactive)
   (let ((identifier (inclue-identifier-at-point)))
-    (if identifier
-        (let ((header (gethash identifier inclue-mappings)))
-          (cond ((null header)
-                 (message "I don't know the header to include for %s" identifier))
-                ((inclue-has-header header)
-                 (message "Header %s already included" header))
-                (t
+    (loop
+     for library-identifier in inclue-active-libraries until
+     (let ((library (gethash library-identifier inclue-library-table)))
+       (when library
+         (let ((header (gethash identifier 
+                                (inclue-library-header-table library))))
+           (when header
+             (if (inclue-has-header header)
+                 (message "Header %s already included" header) ;; terminate
+               (progn
                  (message "Including header %s" header)
-                 (inclue-add-header header))))
-      (message "No recognisable identifier at point"))))
+                 (inclue-add-header header))))))))))
 
 (defun inclue-show-documentation-for-identifier-at-point ()
-  "Show the documentation string for the identifier at point."
+  "Show the documentation string for the identifier at point, if we have it."
   (interactive)
   (let ((identifier (inclue-identifier-at-point)))
-    (message identifier)
-    (if identifier
-        (let ((documentation (gethash identifier inclue-documentation)))
-          (if documentation
-              (message documentation)
-              (message "No documentation for %s" identifier)))
-      (message "No recognisable identifier at point"))))
+    (loop for library-identifier in inclue-active-libraries do
+          (let ((library (gethash library-identifier inclue-library-table)))
+            (when library
+              (let ((documentation
+                     (gethash identifier 
+                              (inclue-library-documentation-table library))))
+                (when documentation
+                  (message documentation))))))))
+
+(defun inclue-activate-library ()
+  "Activate registered libraries in the current buffer."
+  (interactive)
+  (let ((library-name
+         (completing-read 
+          "Activate library: "
+          (loop for identifier being the hash-keys of inclue-library-table 
+                collect (symbol-name identifier))
+          nil
+          t)))
+    (add-to-list 'inclue-active-libraries (intern library-name))))
+
+(defun inclue-construct-ac-candidates ()
+  "Build a list of competions from all currently active libraries."
+  ;; TODO find out how to plug this into auto-complete.el
+  (loop for library being the hash-value of inclue-library-table
+        when (memq (inclue-library-identifier library) inclue-active-libraries)
+        append (inclue-library-names library)))
 
 (provide 'inclue)
 
